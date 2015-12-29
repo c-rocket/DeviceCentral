97b7ec11e982f9fd834475b5f29ca7eeb86a5070
ccrock <cam.crockett.12@gmail.com>
2015-12-29 13:20:18 -0700

    Updating test locations

--- IotDeviceCentral/src/test/java/com/oracle/iot/devicecentral/service/IoTDeviceServiceTest.java
+++ IotDeviceCentral/src/test/java/com/oracle/iot/devicecentral/service/IoTDeviceServiceTest.java
@@ -1,4 +1,4 @@
-package com.oracle.iot.devicecentral.dao;
+package com.oracle.iot.devicecentral.service;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,6 +14,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.oracle.iot.devicecentral.dao.IotDeviceDao;
 import com.oracle.iot.devicecentral.service.IoTDeviceService;
 
 @RunWith(MockitoJUnitRunner.class)
